52f9f28a4c9605eb689707884b5cf8b58a4708ae
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2022-05-18 01:24:49 +0900

    implement perception feature pooling

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 from torchvision.models import vgg16
-from scipy.interpolate import interp2d # TODO: add scipy to environment.yml
 from pytorch3d.io.ply_io import load_ply
 from pytorch3d.structures import Meshes
 
@@ -40,7 +39,6 @@ class P2M(nn.Module):
         self.g_resnet3 = create_g_resnet()
 
     # @return pixel coordinates in 224x224 input image
-    # TODO: add image or image dimension in parameter
     def image_project(self, coordinates, vgg16_features, camera_c, camera_f):
         projection_matrix = torch.mm(
             torch.tensor([
@@ -53,12 +51,12 @@ class P2M(nn.Module):
                 [0, 1, 0, 0],
                 [0, 0, 1, 0.8],
             ])
-        )
+        ).to("cuda")
         projection_matrix = projection_matrix.repeat(coordinates.shape[0], 1, 1)
 
         coordinates = torch.concat([
             coordinates,
-            torch.ones(coordinates.shape[0], 1)
+            torch.ones(coordinates.shape[0], 1).to("cuda")
         ], 1)
         coordinates = torch.reshape(coordinates, (coordinates.shape[0], coordinates.shape[1], 1))
 
@@ -73,40 +71,59 @@ class P2M(nn.Module):
 
         return pixel_coordinates
 
-    def pool_features(self, coordinates_2d, vgg16_features):
-        def bilinear_interpolation(coordinates_2d, vgg16_feature):
-            # Get dimension of vgg16 feature
-            dimension = vgg16_feature.shape[0]
-            # Rescale coordinate with the dimension
-            coordinates_2d = coordinates_2d * dimension
-            # Call interp2d, TODO: make shape consistent
-            return interp2d(coordinates_2d[0], coordinates_2d[1], vgg16_feature)
+    def bilinear_interpolation(self, x, y, feature, image_size):
+        dimension = feature.shape[1]
+        x = x * dimension / image_size
+        y = y * dimension / image_size
+
+        x1 = torch.floor(x)
+        x2 = x1 + 1
+        y1 = torch.floor(y)
+        y2 = y1 + 1
+
+        feature11 = feature[:, x1.to(torch.long), y1.to(torch.long)].transpose(0, 1)
+        feature12 = feature[:, x1.to(torch.long), y2.to(torch.long)].transpose(0, 1)
+        feature21 = feature[:, x2.to(torch.long), y1.to(torch.long)].transpose(0, 1)
+        feature22 = feature[:, x2.to(torch.long), y2.to(torch.long)].transpose(0, 1)
+
+        w11 = (x2 - x) * (y2 - y)
+        w12 = (x2 - x) * (y - y1)
+        w21 = (x - x1) * (y2 - y)
+        w22 = (x - x1) * (y - y1)
+
+        result = torch.reshape(w11.broadcast_to(feature11.shape[:2]), feature11.shape) * feature11
+        result += torch.reshape(w12.broadcast_to(feature12.shape[:2]), feature12.shape) * feature12
+        result += torch.reshape(w21.broadcast_to(feature21.shape[:2]), feature21.shape) * feature21
+        result += torch.reshape(w22.broadcast_to(feature22.shape[:2]), feature22.shape) * feature22
+        
+        return result
+
+    def pool_features(self, coordinates_2d, vgg16_features, image_size):
+        x, y = torch.split(coordinates_2d, 1, 1)
             
         return [
-            bilinear_interpolation(coordinates_2d, vgg16_features[0]),
-            bilinear_interpolation(coordinates_2d, vgg16_features[1]),
-            bilinear_interpolation(coordinates_2d, vgg16_features[2]),
+            self.bilinear_interpolation(x, y, vgg16_features[0][0], image_size),
+            self.bilinear_interpolation(x, y, vgg16_features[1][0], image_size),
+            self.bilinear_interpolation(x, y, vgg16_features[2][0], image_size),
         ]
 
-    def pool_perception_feature(self, mesh, vgg16_features, camera_c, camera_f):
+    def pool_perception_feature(self, mesh, vgg16_features, camera_c, camera_f, image_size):
         coordinates = mesh.verts_list()[0]
         pixel_coordinates = self.image_project(coordinates, vgg16_features, camera_c, camera_f)
-        perception_features = self.pool_features(pixel_coordinates, vgg16_features)
+        perception_features = self.pool_features(pixel_coordinates, vgg16_features, image_size)
 
-        # TODO: concat three perception features
-        return perception_features
+        return torch.concat(perception_features, 1)
 
     def generate_initial_mesh(self):
-        # TODO: implement
         vertices, faces = load_ply(self.ellipsoid_path)
-        return Meshes(verts=[vertices], faces=[faces])
+        return Meshes(verts=[vertices], faces=[faces]).cuda()
 
     def unpool_graph(self, graph):
         raise NotImplementedError()
 
-    def deform_mesh(self, mesh, vgg16_features, camera_c, camera_f, g_resnet):
+    def deform_mesh(self, mesh, vgg16_features, camera_c, camera_f, g_resnet, image_size):
         # coordinates, feature = mesh
-        perception_feature = self.pool_perception_feature(mesh, vgg16_features, camera_c, camera_f)
+        perception_feature = self.pool_perception_feature(mesh, vgg16_features, camera_c, camera_f, image_size)
 
         # TODO: concat perception_feature with feature
         feature_input = None
@@ -117,6 +134,7 @@ class P2M(nn.Module):
     # @param image - 137x137 image
     # @param camera - camera intrinsic and extrinsic matrices
     def forward(self, image, camera_c, camera_f):
+        _, __, image_size, ___ = image.shape
         self.vgg16(image)
 
         vgg16_features = [
@@ -127,12 +145,12 @@ class P2M(nn.Module):
 
         mesh = self.generate_initial_mesh()
 
-        mesh = self.deform_mesh(mesh, vgg16_features, camera_c, camera_f, self.g_resnet1)
+        mesh = self.deform_mesh(mesh, vgg16_features, camera_c, camera_f, self.g_resnet1, image_size)
         mesh = self.unpool_graph(mesh)
 
-        mesh = self.deform_mesh(mesh, vgg16_features, camera_c, camera_f, self.g_resnet2)
+        mesh = self.deform_mesh(mesh, vgg16_features, camera_c, camera_f, self.g_resnet2, image_size)
         mesh = self.unpool_graph(mesh)
 
-        mesh = self.deform_mesh(mesh, vgg16_features, camera_c, camera_f, self.g_resnet3)
+        mesh = self.deform_mesh(mesh, vgg16_features, camera_c, camera_f, self.g_resnet3, image_size)
 
         return mesh
