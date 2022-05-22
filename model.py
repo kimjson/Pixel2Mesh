43a2ee375dc9769f31b213bef21e15daaa7737bf
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2022-05-22 20:28:48 +0900

    Tiny refactoring

--- model.py
+++ model.py
@@ -167,11 +167,10 @@ class P2M(nn.Module):
             newFaces = torch.cat((newFaces, torch.tensor([[i2,i4,i5]]).cuda()),0)
             newFaces = torch.cat((newFaces, torch.tensor([[i3,i5,i6]]).cuda()),0)
             newFaces = torch.cat((newFaces, torch.tensor([[i5,i4,i6]]).cuda()),0)
-        verticesTemp = vertices[:, :3]
-        shapesTemp = vertices[:, 3:]
-        vertices = verticesTemp
-        shape_features = shapesTemp
-        # TODO: split vertices into coordinates and shape_features
+            
+        shape_features = vertices[:, 3:]
+        vertices = vertices[:, :3]
+        
         return Meshes(verts=[vertices], faces=[newFaces]).cuda(), shape_features
 
     def deform_mesh(self, mesh, shape_features, vgg16_features, camera_c, camera_f, g_resnet, image_size):
