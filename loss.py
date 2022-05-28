3d9b5556cfe7f8bc823248057dc4ffb9af3397a4
JeyhunZakaryayev <zakaryayev@kaist.ac.kr>
2022-05-28 22:36:22 +0900

    normal_loss 1st version

--- loss.py
+++ loss.py
@@ -1,3 +1,4 @@
+from unittest import result
 from pytorch3d.loss import chamfer_distance 
 from pytorch3d.ops import knn_points
 import torch
@@ -6,9 +7,13 @@ def p2m_loss (prediction, g_truth, neighbours):
     loss = normal_loss(prediction, g_truth, neighbours) + chamferloss
     return loss
 
-def normal_loss(prediction, g_truth, neighbours):
-    knn = knn_points(prediction, g_truth, return_nn = True).knn
-    closes_vertices = torch.reshape(knn, (1,10,3))
-    edges = torch.empty().cuda()
-    for neighbour in neighbours : 
-
+#TODO : normalize vectors
+def normal_loss(prediction, g_truth, neighbours, surface_normals):
+    nn_indices = knn_points(prediction, g_truth, return_nn = True).idx
+    result = 0
+    for index, neighbour in enumerate(neighbours) : 
+        norm = surface_normals[nn_indices[index]]
+        for vert in neighbour : 
+            edge = prediction[vert] - prediction[index]
+            result +=torch.dot(edge, norm)
+    return result
