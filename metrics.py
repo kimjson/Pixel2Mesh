f54c13c6dfe34c7cdaacc40296ddbdf34695987e
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2022-05-31 22:05:28 +0900

    Deal with th edge case where precision and recall both 0 when calculating f1 score

--- metrics.py
+++ metrics.py
@@ -5,9 +5,11 @@ def f_score(prediction,g_truth, tau= 1e-4):
     true_positive = knn_points(prediction, g_truth).dists
     true_positive = torch.count_nonzero(true_positive<tau)
     false_positive = prediction.size()[1] - true_positive
-    false_negative = knn_points(g_truth, prediction)
+
+    false_negative = knn_points(g_truth, prediction).dists
     false_negative = g_truth.size()[1] - torch.count_nonzero(false_negative<tau)
+
     precision = true_positive/(false_positive+true_positive)
     recall = true_positive/(false_negative + true_positive)
-    f1_score = recall*precision*2/(recall+precision)
+    f1_score = (recall*precision*2/(recall+precision)).item() if (precision + recall) > 0 else 0
     return f1_score
