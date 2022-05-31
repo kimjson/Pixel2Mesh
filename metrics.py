f51007bb85b78e450a6d84b40600d4adc89f91f3
JeyhunZakaryayev <zakaryayev@kaist.ac.kr>
2022-05-31 21:50:37 +0900

    added f1-score

---
+++ metrics.py
@@ -0,0 +1,13 @@
+from pytorch3d.ops import knn_points
+import torch
+
+def f_score(prediction,g_truth, tau= 1e-4):
+    true_positive = knn_points(prediction, g_truth).dists
+    true_positive = torch.count_nonzero(true_positive<tau)
+    false_positive = prediction.size()[1] - true_positive
+    false_negative = knn_points(g_truth, prediction)
+    false_negative = g_truth.size()[1] - torch.count_nonzero(false_negative<tau)
+    precision = true_positive/(false_positive+true_positive)
+    recall = true_positive/(false_negative + true_positive)
+    f1_score = recall*precision*2/(recall+precision)
+    return f1_score
