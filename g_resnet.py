Fix nan gradient bug, logic error in normal loss

g_resnet.py
@@ -1,7 +1,15 @@
 from torch.nn import Module, ReLU, ModuleList
+from torch.nn.init import xavier_uniform_
 from pytorch3d.ops import GraphConv
 
 from graph_convolution import GraphConvolution
+
+def create_graph_conv(input_dim, output_dim):
+    graph_conv = GraphConv(input_dim, output_dim).to("cuda")
+    # xavier_uniform_(graph_conv.w0.weight, gain=0.1)
+    # xavier_uniform_(graph_conv.w1.weight, gain=0.1)
+
+    return graph_conv
 
 class GResNet(Module) : 
     def __init__(self, inputDim, outputDim):
@@ -9,7 +17,7 @@ class GResNet(Module) :
         
         self.inputDim = inputDim
         self.outputDim = outputDim
-        self.gcnlayers = [GraphConv(inputDim, outputDim).to("cuda")]+[GraphConv(outputDim, outputDim).to("cuda") for i in range(12)]+[GraphConv(outputDim, 3).to("cuda")]
+        self.gcnlayers = [create_graph_conv(inputDim, outputDim)]+[create_graph_conv(outputDim, outputDim) for i in range(12)]+[create_graph_conv(outputDim, 3)]
         self.gcnlayers = ModuleList(self.gcnlayers)
 
     def forward(self, edges, shape_features):

loss.py
@@ -1,4 +1,3 @@
-from unittest import result
 from pytorch3d.loss import chamfer_distance 
 from pytorch3d.ops import knn_points
 from torch.nn.functional import mse_loss, normalize
@@ -29,22 +28,23 @@ def normal_loss(prediction, g_truth, g_truth_normals, adjacency_matrix, edges):
     nn_indices = knn_points(prediction, g_truth, return_nn = True).idx
     nn_indices = nn_indices[0]
     vertices = prediction[0]
-    
-    epsilon = 1e-12
 
-    g_truth_normals = normalize(g_truth_normals[0], eps=epsilon)[nn_indices]
     max_edge_length_value = max_edge_length(vertices, edges)
+    epsilon = torch.tensor(1e-12, device=torch.device("cuda"))
+    normalizer = torch.maximum(epsilon, max_edge_length_value)
 
     num_vertices, num_coordinates = vertices.shape
 
     neighbors = torch.zeros((num_vertices, num_vertices, num_coordinates), device=torch.device('cuda'))
-    neighbors[edges[:, 0], edges[:, 1]] = vertices[edges[:, 1]] - vertices[edges[:, 0]]
-    neighbors[edges[:, 1], edges[:, 0]] = vertices[edges[:, 0]] - vertices[edges[:, 1]]
+    neighbors[edges[:, 0], edges[:, 1]] = (vertices[edges[:, 1]] - vertices[edges[:, 0]]) / normalizer
+    neighbors[edges[:, 1], edges[:, 0]] = (vertices[edges[:, 0]] - vertices[edges[:, 1]]) / normalizer
+
+    g_truth_normals = normalize(g_truth_normals[0], eps=epsilon)[nn_indices]
 
     result = neighbors @ g_truth_normals.reshape((num_vertices, num_coordinates, 1))
-    result = result.sum(dim=1).sum()
+    result = torch.abs(result).sum(dim=1).mean()
 
-    return abs(result / max(epsilon, max_edge_length_value))
+    return result
 
 
 def max_edge_length(vertices, edges):

model.py
@@ -68,13 +68,10 @@ class P2M(nn.Module):
         coordinates = torch.reshape(coordinates, (coordinates.shape[0], coordinates.shape[1], 1))
 
         pixel_coordinates = torch.bmm(projection_matrix, coordinates)
-        pixel_coordinates = torch.reshape(pixel_coordinates, (pixel_coordinates.shape[0], pixel_coordinates.shape[1]))
+        s_values = pixel_coordinates[:, 2]
+        pixel_coordinates = pixel_coordinates.flatten(1, 2)
 
-        s_values = torch.reshape(pixel_coordinates[:, 2], (pixel_coordinates.shape[0], 1)).repeat(1, 3)
-
-        pixel_coordinates = pixel_coordinates / s_values
-
-        pixel_coordinates = pixel_coordinates[:, :-1]
+        pixel_coordinates = pixel_coordinates[:, :-1] / (s_values + (1e-12))
 
         return pixel_coordinates
 